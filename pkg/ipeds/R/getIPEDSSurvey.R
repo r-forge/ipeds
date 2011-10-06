e73b055c7ff183dce71d2094e8cd4594ecce31cd
jbryer (Research Associate ) <jason@bryer.org>
2011-10-06 00:33:59 +0000

    Updated downloadAllSurveys with tryCatch so if a survey doesn't exit for the given year, the download will not stop.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipeds@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ipeds/R/getIPEDSSurvey.R
+++ pkg/ipeds/R/getIPEDSSurvey.R
@@ -1,22 +1,42 @@
-getIPEDSSurvey <- function(surveyId, year) {
+#' This function will return the IPEDS survey data for the given survey and year. Note that the first time this function is invoked it will attempt to download the data file from the IPEDS website.
+#'
+#' @title getIPEDSSurvey
+#' @param surveyId The survey ID of the dictionary to display (i.e. surveys$SurveyID)
+#' @param year The year of the dictionary to display.
+#' @param dir the directory to download the data to. Defaults to the package directory
+#' @export
+getIPEDSSurvey <- function(surveyId, year, dir=system.file(package="ipeds")) {
 	s = surveys[which(surveys$SurveyID==surveyId),]
 	if(nrow(s) != 1) {
 		stop(paste('IPEDS survey with id', surveyId, 'not found'))
 	}
-	dir = system.file(package="ipeds")
+	#dir = system.file(package="ipeds")
 	file = paste(s[1,'DataFilePre'], year, s[1,'DataFilePost'], sep='')
 	dest = paste(dir, "/data/downloaded/", file, '.csv', sep="")
 	if(!file.exists(dest)) {
-		r = downloadIPEDSSurvey(surveyId, year)
+		r = try(downloadIPEDSSurvey(surveyId, year, dir=dir), FALSE)
+  		if(class(r) == "try-error") {
+  			r = NULL
+  		}
 	} else {
 		r = read.csv(dest)
 	}
+ 	if(!is.null(r)) {
+	 	names(r) = tolower(names(r))
+ 	}
 	r
 }
 
-downloadIPEDSSurvey <- function(surveyId, year) {
+#' Downloads a given IPEDS survey data file.
+#' 
+#' @title downloadIPEDSSurvey
+#' @param surveyId the survey ID from the surveys data frame
+#' @param year the year of the survey
+#' @param dir the directory to download the data to. Defaults to the package directory
+#' @export
+downloadIPEDSSurvey <- function(surveyId, year, dir=system.file(package="ipeds")) {
 	s = surveys[which(surveys$SurveyID==surveyId),]
-	dir = system.file(package="ipeds")
+	#dir = system.file(package="ipeds")
 	file = paste(s[1,'DataFilePre'], formatYear(surveyId, year), s[1,'DataFilePost'], sep='')
 	url = paste(ipedsDataUrl, file, '.zip', sep='')
 	dir.create(paste(dir, '/data/downloaded/', sep=''), showWarnings=FALSE)
@@ -42,8 +62,14 @@ formatYear <- function(surveyId, year) {
 	year
 }
 
-downloadAllSurveys <- function(year) {
+#' Downloads all available surveys for the given year.
+#' 
+#' @title downloadAllSurveys
+#' @param year the year to download
+#' @param ... other parameters passed to \code{\link{downloadIPEDSSurvey}}
+#' @export
+downloadAllSurveys <- function(year, ...) {
 	for(i in 1:nrow(surveys)) {
-		downloadIPEDSSurvey(surveys[i,'SurveyID'], year)
+		tryCatch(downloadIPEDSSurvey(surveys[i,'SurveyID'], year, ...), error=function(e) { warning(paste("Error downloading survey:",surveys[i,'SurveyID'], surveys[i,'Title'])) })
 	}
 }
